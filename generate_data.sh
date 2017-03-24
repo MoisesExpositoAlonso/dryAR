1c117e0db6cefdc32d8b99f19c07526b5c6a6de4
mexposito <moises.exposito-alonso@tuebingen.mpg.de>
2017-03-24 12:41:18 +0100

    Included generated data bash script to produce R objects of data/ from raw data and generating scripts from data-raw/ an

---
+++ generate_data.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Generate the data of field package stored under data/ from the source dataset in data-raw/
+
+Rscript  data-raw/gen_quickpot_locations.R
+Rscript  data-raw/gen_accessionlist.R
+Rscript  data-raw/gen_genotypereplocations.R
+Rscript  data-raw/gen_floweringdata.R
+Rscript  data-raw/gen_harvesting.R
+
+Rscript  data-raw/gen_dataextra.R
+
+# When there have been changes in some of the above, to refresh the master data, run:
+Rscript  data-raw/refresh_data.R
+
